a9be0da27c0162fc8e3404ce82f457896df0238a
John Doe <johndoe@email.com>
2024-12-22 19:27:24 +0000

    moved secrets outside the code

--- continente_price_tracker/src/sql/main.py
+++ continente_price_tracker/src/sql/main.py
@@ -4,18 +4,19 @@ from pingo_doce.main import preprocess_and_insert_data_pingo_doce
 from continente.main import preprocess_and_insert_data_continente
 from utils import concat_csv_from_supabase
 
-data_auchan = concat_csv_from_supabase("auchan")
-data_continente = concat_csv_from_supabase("continente")
-data_pingo_doce = concat_csv_from_supabase("pingo_doce")
+# read data from supabase buckets
+data_auchan = concat_csv_from_supabase(folder_name= "auchan")
+data_continente = concat_csv_from_supabase(folder_name= "continente")
+data_pingo_doce = concat_csv_from_supabase(folder_name= "pingo_doce")
 
 # Initialize the database interface
 # No need to specify db_name for PostgreSQL connection
 db_interface = ProductDatabaseInterface()
 
 # Path to the Parquet file
-parquet_file = 'data/raw/auchan_combined.parquet'
-continente_file = 'data/raw/continente.parquet'
-pingo_doce_file = 'data/raw/pingo_doce.parquet'
+# parquet_file = 'data/raw/auchan_combined.parquet'
+# continente_file = 'data/raw/continente.parquet'
+# pingo_doce_file = 'data/raw/pingo_doce.parquet'
 
 # Step 4: Preprocess the data and insert into the database
 preprocess_and_insert_data_auchan(data_auchan, db_interface)
