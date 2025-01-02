a32ba70f161e6f9b3925aee3a58dc90f98b53775
John Doe <johndoe@email.com>
2025-01-02 15:44:28 +0000

    added CORS middlware because of frontend development

--- api/app/main.py
+++ api/app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from core.config import settings
 from api.v1.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 # import uvicorn
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.VERSION,
     openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
