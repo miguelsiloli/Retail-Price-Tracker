cc6b34a7f8976fc47217372213a80afc8b7e3d2d
miguel <miguel98oliveira@gmail.com>
2025-03-23 18:15:45 +0000

    concurrency execution was deprecated

--- continente_price_tracker/src/object_storage/main_concurrency.py
+++ continente_price_tracker/src/object_storage/main_concurrency.py
@@ -1,25 +1,25 @@
 import concurrent.futures
-from continente.catalog import process_and_save_categories
-from pingo_doce.pingo_doce import parse_and_save_all_categories
-from auchan.auchan import save_data_for_all_cgids
+from scrapper.catalog import process_and_save_categories
+from scrapper.pingo_doce import parse_and_save_all_categories
+from scrapper.auchan import save_data_for_all_cgids
 
 def main():
     # Define the tasks you want to run in parallel
     tasks = [
-        ("Continente", process_and_save_categories, {"base_path": "data/raw/continente"}),
+        ("Continente", process_and_save_categories, {}),
         ("Pingo Doce", parse_and_save_all_categories, {"categories": [
             "pingo-doce-lacticinios", "pingo-doce-bebidas",
             "pingo-doce-frescos-embalados", "pingo-doce-higiene-e-beleza",
             "pingo-doce-maquinas-e-capsulas-de-cafe", "pingo-doce-mercearia",
-            "pingo-doce-refeicoes-prontas"
+            "pingo-doce-refeicoes-prontas", "pingo-doce-cozinha-e-limpeza", 
+            "pingo-doce-congelados"
         ]}),
         ("Auchan", save_data_for_all_cgids, {
             "cgid_list": [
                 "alimentacao-", "biologico-e-escolhas-alimentares",
-                "limpeza-da-casa-e-roupa", "bebidas-e-garrafeira", "marcas-auchan", 
-                "produtos-frescos", "Páginasbe_Antimanchas", "Páginasbe_Antiidade",
-                "Páginasbe_Acne", "produtos-solares", "multivitaminicos", "PaginaSBE_pelesecaatopica",
-                "maquilhagem"
+                "limpeza-da-casa-e-roupa", "bebidas-e-garrafeira", "marcas-auchan",
+                "saude-e-bem-estar", "brinquedos-papelaria-livraria",
+                "Feira-de-Beleza"
             ],
             "prefn1": "soldInStores",
             "prefv1": "000",
@@ -28,11 +28,11 @@ def main():
             "base_path": "data/raw/auchan"
         })
     ]
-
+    
     # Use ThreadPoolExecutor to run tasks in parallel
     with concurrent.futures.ThreadPoolExecutor() as executor:
         future_to_task = {executor.submit(task[1], **task[2]): task[0] for task in tasks}
-
+        
         for future in concurrent.futures.as_completed(future_to_task):
             task_name = future_to_task[future]
             try:
